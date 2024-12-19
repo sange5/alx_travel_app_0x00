728bcb9cf60de0b21cf619135977d7cd45cb5697
sange5 <163669819+sange5@users.noreply.github.com>
2024-12-19 15:38:46 +0300

    Delete alx_travel_app directory

--- alx_travel_app/listings/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ListingListView
-
-urlpatterns = [
-    path('listings/', ListingListView.as_view(), name='listings'),
-]
--- alx_travel_app/listings/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.generics import ListCreateAPIView
-from .models import Listing
-from .serializers import ListingSerializer
-
-class ListingListView(ListCreateAPIView):
-    queryset = Listing.objects.all()
-    serializer_class = ListingSerializer
